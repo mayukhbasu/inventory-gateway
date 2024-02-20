94f41bb41845a83a7800c8d58d14e5cc912f0d3e
Mayukh Basu <roxy.firebird@gmail.com>
2024-02-20 21:34:35 +0530

    first commit of the internet gateway

---
+++ src/main/java/com/inventory/gateway/InventoryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryGatewayApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/inventory/gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.inventory.gateway.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig{
+	
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+        .authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests
+                .requestMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+        )
+        .logout(logout -> logout
+            .logoutSuccessUrl("/").permitAll()
+        )
+        .oauth2Login(null);
+		return http.build();
+	}
+}
+
+
